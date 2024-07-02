fddae92cebbe8f0610e8605df1bb59d593a8216d
Ramadhan <dev.ramddd@gmail.com>
2024-07-02 14:16:50 +0700

    Add Functional Form

--- Controllers/BalanceController.cs
+++ Controllers/BalanceController.cs
@@ -1,4 +1,5 @@
 using AspNetBalanceSheet.Data;
+using AspNetBalanceSheet.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetBalanceSheet.Controllers
@@ -18,6 +19,52 @@ namespace AspNetBalanceSheet.Controllers
             var transactions = _context.BalanceTransactions.ToList();
 
             return View((balances, transactions));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateTransaction(DateTime date, bool isFlip, string transferFrom, string transferTo, decimal amount)
+        {
+            if (ModelState.IsValid)
+            {
+                var creditTransaction = new BalanceTransaction
+                {
+                    Date = date,
+                    IsFlip = isFlip,
+                    Account = transferFrom,
+                    TransactionType = "CREDIT",
+                    Amount = amount * -1
+                };
+
+                var debitTransaction = new BalanceTransaction
+                {
+                    Date = date,
+                    IsFlip = isFlip,
+                    TransactionType = "DEBIT",
+                    Account = transferTo,
+                    Amount = amount
+                };
+
+                var balanceFrom = _context.Balances.FirstOrDefault(b => b.Account == transferFrom);
+                if (balanceFrom != null)
+                {
+                    balanceFrom.Amount += creditTransaction.Amount;
+                }
+
+                // Update Balance for transferTo
+                var balanceTo = _context.Balances.FirstOrDefault(b => b.Account == transferTo);
+                if (balanceTo != null)
+                {
+                    balanceTo.Amount += debitTransaction.Amount;
+                }
+
+                _context.BalanceTransactions.Add(creditTransaction);
+                _context.BalanceTransactions.Add(debitTransaction);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Migrations/20240702065458_TestChangeData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetBalanceSheet.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestChangeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BalanceTransactions",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 7, 2, 13, 54, 57, 66, DateTimeKind.Local).AddTicks(7083));
+
+            migrationBuilder.UpdateData(
+                table: "BalanceTransactions",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 7, 2, 13, 54, 57, 66, DateTimeKind.Local).AddTicks(7096));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BalanceTransactions",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 7, 2, 12, 4, 11, 738, DateTimeKind.Local).AddTicks(1281));
+
+            migrationBuilder.UpdateData(
+                table: "BalanceTransactions",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 7, 2, 12, 4, 11, 738, DateTimeKind.Local).AddTicks(1307));
+        }
+    }
+}
